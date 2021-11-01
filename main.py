293fa6564a3ab2bff8581fbd79f0ddb8b026460f
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-11-01 18:11:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+****** targil 1:
+  
+fruits = ['apple', 'banana', 'cherry', 'kiwi', 'mango']
+new_fr = [i for i in fruits  if i.count('a') > 0 ]
+print(new_fr)
+
+
+****** targil 2:
+  
+some_list = [1,4,7,12,19,22, 23, 26]
+sort_list = [(f'{x} is even') if x % 2 == 0 else (f'{x} is odd') for x in some_list  ]
+print(sort_list)
+
+
+****** targil 3:
+  
